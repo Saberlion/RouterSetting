3e16dbb69a98c0b9f80154c9444ef2aeaf1382b4
Saberlion <atpx4869>
2014-04-03 21:59:01 +0800

    create project

---
+++ settings.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# IP for the router
+ip = '192.168.1.1'
+# 路由器登录的用户名和密码
+login_user = 'admin'
+login_pw = 'admin'
+
+#WAN STATIC IP
+#WAN静态IP
+wan_static_ip = '59.69.64.224'
+wan_static_mask='255.255.255.0'
+wan_static_gateway='59.69.64.1'
+wan_static_mtu='1500' #MTU default value 默认值
+wan_static_dnsserver='202.114.88.10'
+wan_static_dnsserver2='0.0.0.0'
+
+#PPPOE
+pppoe_usename=''
+pppoe_password=''
+
+#wifi password
+wifi_psw='remirp++c'#不少于8位数
+
