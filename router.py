64d09f4e12d66016f84457204f9a8dda0e19fffd
saberlion <fxre@qq.com>
2014-04-08 00:01:52 +0800

    add setting wifi password

--- router.py
+++ router.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
-
+# -*- coding: utf8 -*-
+#
 import urllib2, base64
 from settings import *
 
 if __name__ == '__main__':  
 
     # 请求地址
-    reboot_url = 'http://' + ip  +'/userRpm/SysRebootRpm.htm?Reboot=%D6%D8%C6%F4%C2%B7%D3%C9%C6%F7'
+    reboot_url = 'http://' + ip+'/userRpm/SysRebootRpm.htm?Reboot=%D6%D8%C6%F4%C2%B7%D3%C9%C6%F7'
     reboot_ref ='/userRpm/SysRebootRpm.htm'
     
     wan_static_url = 'http://' + ip +'/userRpm/WanStaticIpCfgRpm.htm?wantype=1&ip='+wan_static_ip+'&mask='+wan_static_mask+'&gateway=' +wan_static_gateway+'&mtu='+wan_static_mtu+'&dnsserver='+wan_static_dnsserver+'&dnsserver2='+wan_static_dnsserver+'&downBandwidth=0&upBandwidth=0&Save=%B1%A3+%B4%E6'
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     pppoe_url ='http://' + ip +'/userRpm/PPPoECfgRpm.htm?wan=0&wantype=2&acc='+pppoe_usename+'&psw='+pppoe_password+'&confirm=' +pppoe_password+'&specialDial=100&SecType=0&sta_ip=0.0.0.0&sta_mask=0.0.0.0&linktype=1&waittime=15&Save=%B1%A3+%B4%E6'
     pppoe_ref ='/userRpm/PPPoECfgRpm.htm'
     
-    wifi_psw_url ='http://' + ip +'/userRpm/WlanSecurityRpm.htm?vapIdx=1&secType=3&pskSecOpt=2&pskCipher=3&pskSecret='+wifi_psw +'&interval=86400&wpaSecOpt=3&wpaCipher=1&radiusIp=&radiusPort=1812&radiusSecret=&intervalWpa=86400&wepSecOpt=3&keytype=1&keynum=1&ke'
+    wifi_psw_url ='http://' + ip +'/userRpm/WlanSecurityRpm.htm?vapIdx=1&secType=3&pskSecOpt=2&pskCipher=3&pskSecret='+wifi_psw +'&interval=86400&wpaSecOpt=3&wpaCipher=1&radiusIp=&radiusPort=1812&radiusSecret=&intervalWpa=86400&wepSecOpt=3&keytype=1&keynum=1&key1=&length1=0&key2=&length2=0&key3=&length3=0&key4=&length4=0&Save=%B1%A3+%B4%E6'
     wifi_psw_ref ='/userRpm/WlanSecurityRpm.htm'
     
     auth = 'Basic ' + base64.b64encode(login_user+':'+login_pw)
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     }
     
     while 1:
-        print '输入序号设置\n1.设置静态IP\n2.设置PPPOE \n3.设置wifi密码\n4.重启路由器\n5.退出'
+        print u'输入序号设置\n1.设置静态IP\n2.设置PPPOE \n3.设置wifi密码\n4.重启路由器\n5.退出'
         choice = input()
         if choice == 1:
             url = wan_static_url
@@ -45,5 +45,12 @@ if __name__ == '__main__':
             continue
         heads = { 'Referer' : 'http://' + ip + ref,'Authorization' : auth }
         request = urllib2.Request(url, None, heads)
-        response = urllib2.urlopen(request)
-        print response.read()
+        try:
+            response = urllib2.urlopen(request)
+        except  urllib2.HTTPError   as e    :
+            print   u'验证错误\n\n'
+            continue
+        except  urllib2.URLError   as e    :
+            print u'连接错误，请确保正确连接到路由器\n\n'
+            continue
+        # print response.read().decode('gb2312').encode('utf8')
--- settings.py
+++ settings.py
@@ -7,17 +7,17 @@ login_pw = 'admin'
 
 #WAN STATIC IP
 #WAN静态IP
-wan_static_ip = '59.69.64.224'
+wan_static_ip = ''#'59.69.64.224'
 wan_static_mask='255.255.255.0'
-wan_static_gateway='59.69.64.1'
+wan_static_gateway=''#'59.69.64.1'
 wan_static_mtu='1500' #MTU default value 默认值
-wan_static_dnsserver='202.114.88.10'
-wan_static_dnsserver2='0.0.0.0'
+wan_static_dnsserver='114.114.114.114'
+wan_static_dnsserver2='8.8.8.8'
 
 #PPPOE
 pppoe_usename=''
 pppoe_password=''
 
 #wifi password
-wifi_psw='remirp++c'#不少于8位数
+wifi_psw=''#不少于8位数
 
